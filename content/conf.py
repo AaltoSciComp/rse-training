bdaa24a291a6d016638da83c2585d89cf636c926
Richard Darst <rkd@zgib.net>
2026-02-14 23:53:48 +0200

    theming

--- content/conf.py
+++ content/conf.py
@@ -13,7 +13,10 @@ author = 'The contributors'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx_lesson',
+              'sphinx_rtd_theme_ext_color_contrast',
+              'sphinx_aaltoscicomp_branding',
+              ]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -23,5 +26,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
